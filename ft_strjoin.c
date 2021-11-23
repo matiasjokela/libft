cc3477e2114f2167453f15442f76633333010033
Matias Jokela <mjokela@c2r5p6.hive.fi>
2021-11-23 14:44:14 +0200

    Fixing NULL protection for ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -16,8 +16,12 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*join;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
+	if (s1 == NULL && s2 != NULL)
+		return (ft_strdup(s2));
+	if (s1 != NULL && s2 == NULL)
+		return (ft_strdup(s1));
 	join = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (join == NULL)
 		return (NULL);
